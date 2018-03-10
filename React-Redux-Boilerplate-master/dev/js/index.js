b8c6397324b0cc5e94ebc65812b49abe6f5c47a6
lei <slayer12135293@hotmail.com>
2018-03-10 01:34:59 +0100

    added logger

--- React-Redux-Boilerplate-master/dev/js/index.js
+++ React-Redux-Boilerplate-master/dev/js/index.js
@@ -1,13 +1,22 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from "react-dom";
-import {createStore} from "redux";
+import {applyMiddleware, createStore} from "redux";
 import {Provider} from "react-redux";
 import allReducers from "./reducers";
 import App from './components/app';
+import { createLogger } from 'redux-logger'
+
+const logger = createLogger({
+    predicate: true,
+    diff: true,
+})
 
 
-const store = createStore(allReducers);
+const store = createStore(allReducers,
+    applyMiddleware(
+        logger,
+    ));
 
 ReactDOM.render(
     <Provider store={store}>
